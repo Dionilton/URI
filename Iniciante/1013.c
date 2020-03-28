99cbc7456b7901ec294cfe5b1ef28329ba023e26
Dionilton Oliveira <47341732+Dionilton@users.noreply.github.com>
2020-03-28 17:42:48 -0300

    Create 1013.c

---
+++ Iniciante/1013.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    int a,b,c,MaiorAB;
+    scanf("%d %d %d", &a,&b,&c);
+    MaiorAB = (a+b+abs(a-b))/2;
+    MaiorAB = (MaiorAB+c+abs(MaiorAB-c))/2;
+    printf("%d eh o maior\n", MaiorAB);
+
+    return 0;
+}
