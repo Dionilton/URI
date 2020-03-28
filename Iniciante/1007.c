035ddd61f51b7ed5d9ae3d651a9fa691a7be561e
Dionilton Oliveira <47341732+Dionilton@users.noreply.github.com>
2020-03-28 04:11:49 -0300

    Create 1007.c

---
+++ Iniciante/1007.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+    int a,b,c,d,diff;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    diff = a*b - c*d;
+    printf("DIFERENCA = %d\n", diff);
+
+    return 0;
+}
