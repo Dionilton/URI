8eb8046720163b0d5084739ddb586e21e4d8258b
Dionilton Oliveira <47341732+Dionilton@users.noreply.github.com>
2020-03-29 03:04:36 -0300

    Create 1037.c

---
+++ Iniciante/1037.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+
+    double n;
+    scanf("%lf", &n);
+    if(n<=25 && n>=0){
+        printf("Intervalo [0,25]\n");
+    }
+    else if(n<=50 && n>0){
+        printf("Intervalo (25,50]\n");
+    }
+    else if(n<=75 && n>0){
+        printf("Intervalo (50,75]\n");
+    }
+    else if(n<=100 && n>0){
+        printf("Intervalo (75,100]\n");
+    }
+    else if(n<0 || n>100){
+        printf("Fora de intervalo\n");
+    }
+
+    return 0;
+}
